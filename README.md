6c534fc41a55ad3977ffe6bfa5483a7c55586814
Aleksey Morarash <aleksey.morarash@proffero.com>
2014-08-04 20:59:07 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Apache Kafka 0.8.x client written on Erlang
+
+This is a very early implementation thus not ready
+for any production use.
---
+++ include/kafka.hrl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%%% File        : kafka.hrl
+%%% Author      : Aleksey Morarash <aleksey.morarash@proffero.com>
+%%% Description : kafka definitions file
+%%% Created     : 04 Aug 2014
+%%%-------------------------------------------------------------------
+
+-ifndef(_KAFKA).
+-define(_KAFKA, true).
+
+%% ----------------------------------------------------------------------
+%% eunit
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+-endif.
