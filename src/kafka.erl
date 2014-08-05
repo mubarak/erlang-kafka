bff0fc876cb70311be125712f93ef9b2865f0da6
Aleksey Morarash <aleksey.morarash@proffero.com>
2014-08-05 17:39:51 +0300

    edoc: add link to a Kafka protocol reference

--- src/kafka.erl
+++ src/kafka.erl
@@ -1,5 +1,9 @@
 %%% @doc
 %%% Erlang Client for the Apache Kafka 0.8.x.
+%%%
+%%% See
+%%% [https://cwiki.apache.org/confluence/display/KAFKA/A+Guide+To+The+Kafka+Protocol]
+%%% for protocol implementation details.
 
 %%% @author Aleksey Morarash <aleksey.morarash@proffero.com>
 %%% @since 04 Aug 2014
